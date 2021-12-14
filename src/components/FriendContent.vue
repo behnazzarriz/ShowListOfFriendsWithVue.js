4cccc1d4809905e7f4cb5eba75bc723d9e336c28
behnaz <behnaz.zarriz@rocket-media.de>
2021-12-14 07:18:36 +0100

    work with Prop-show friends

---
+++ src/components/FriendContent.vue
@@ -0,0 +1,37 @@
+<template>
+<li>
+  <h2>{{ name }}</h2>
+  <button @click="toggleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+  <ul v-if="detailsAreVisible">
+    <li><strong>Phone:</strong>{{phoneNumber}}</li>
+    <li><strong>Email:</strong>{{email}}</li>
+  </ul>
+</li>
+</template>
+<script>
+export default {
+  props:[
+    'name',
+    'phoneNumber',
+    'email'
+  ],
+data(){
+  return{
+    detailsAreVisible:false,
+    friend:
+      {
+        id:'Manuel',
+        name:'Manuel Lorenz',
+        phone:'0123 45658 90',
+        email:'manuel@gmail.com'
+
+      }
+  }
+},
+methods:{
+  toggleDetails(){
+    this.detailsAreVisible=!this.detailsAreVisible;
+  }
+}
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import FriendContent from "./components/FriendContent";
+const app=createApp(App);
+app.component('friend-contact',FriendContent);
+app.mount('#app');
 
-createApp(App).mount('#app')
