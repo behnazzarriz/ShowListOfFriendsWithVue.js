05481bbd9241b1a42ac2ec3f303cc4f68c18d767
behnaz <behnaz.zarriz@rocket-media.de>
2021-12-21 06:48:56 +0100

    add new Contact

---
+++ src/components/NewContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="addNewContact">
+    <div>
+      <label> Name :</label>
+      <input type="text" v-model="name">
+    </div>
+    <div>
+      <label>Phone :</label>
+      <input type="text" v-model="phone">
+    </div>
+    <div>
+      <label>Email :</label>
+      <input type="text" v-model="email">
+    </div>
+    <button>add new contact</button>
+  </form>
+</template>
+<script>
+export default{
+emits:['add-contact'],
+data(){
+  return{
+    name:'',
+    phone:'',
+    email:''
+  }
+},
+methods:{
+  addNewContact(){
+    this.$emit('add-contact',this.name,this.phone,this.email);
+  }
+}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import FriendContent from "./components/FriendContent";
+import NewContact from "./components/NewContact";
 const app=createApp(App);
 app.component('friend-contact',FriendContent);
+app.component('new-contact',NewContact);
 app.mount('#app');
 
